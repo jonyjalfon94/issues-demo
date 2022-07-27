0c04c9daf04014e9895d4142d8c478bdb7b04d59
Jonathan Jalfon <jonyjalfon94@gmail.com>
2022-07-27 19:05:07 +0300

    test

--- scripts/pullRequests.js
+++
@@ -1,9 +0,0 @@
-const issueBody = `${{ github.event.issue.body }}`;
-const prUrls = issueBody.match(/(http|https):\/\/[a-zA-Z0-9./?=_%:-]*/g).sort()
-console.log(prUrls);
-
-prUrls.forEach(url => {
-  console.log("PR URL: " + url);
-  const prData = exec.exec('gh pr view ' + url + ' --json headRefName,headRepository');
-  console.log(prData)
-})
